2b05e48e4cdddbfca017a4e12b9d0b8d14a71670
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2020-02-24 21:19:37 +0300

    Added LongestRootFirstDisambiguation.

---
+++ src/main/java/MorphologicalDisambiguation/LongestRootFirstDisambiguation.java
@@ -0,0 +1,61 @@
+package MorphologicalDisambiguation;
+
+import MorphologicalAnalysis.FsmParse;
+import MorphologicalAnalysis.FsmParseList;
+
+import java.util.ArrayList;
+
+public class LongestRootFirstDisambiguation implements MorphologicalDisambiguator{
+
+    /**
+     * Train method implements method in {@link MorphologicalDisambiguator}.
+     *
+     * @param corpus {@link DisambiguationCorpus} to train.
+     */
+    @Override
+    public void train(DisambiguationCorpus corpus) {
+    }
+
+    /**
+     * The disambiguate method gets an array of fsmParses. Then loops through that parses and finds the longest root
+     * word. At the end, gets the parse with longest word among the fsmParses and adds it to the correctFsmParses
+     * {@link ArrayList}.
+     *
+     * @param fsmParses {@link FsmParseList} to disambiguate.
+     * @return correctFsmParses {@link ArrayList} which holds the parses with longest root words.
+     */
+    @Override
+    public ArrayList<FsmParse> disambiguate(FsmParseList[] fsmParses) {
+        FsmParse bestParse;
+        int maxLength;
+        ArrayList<FsmParse> correctFsmParses = new ArrayList<>();
+        for (FsmParseList fsmParseList : fsmParses) {
+            maxLength = -1;
+            bestParse = null;
+            for (int i = 0; i < fsmParseList.size(); i++){
+                if (fsmParseList.getFsmParse(i).getWord().getName().length() > maxLength){
+                    maxLength = fsmParseList.getFsmParse(i).getWord().getName().length();
+                    bestParse = fsmParseList.getFsmParse(i);
+                }
+            }
+            if (bestParse != null){
+                correctFsmParses.add(bestParse);
+            }
+        }
+        return correctFsmParses;
+    }
+
+    /**
+     * Overridden saveModel method to save a model.
+     */
+    @Override
+    public void saveModel() {
+    }
+
+    /**
+     * Overridden loadModel method to load a model.
+     */
+    @Override
+    public void loadModel() {
+    }
+}

d988d0547c185035e72c234f85d60a3922f85ff8
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-11-20 19:21:57 +0300

    Updated cases.

--- src/main/java/MorphologicalDisambiguation/AutoDisambiguator.java
+++ src/main/java/MorphologicalDisambiguation/AutoDisambiguator.java
@@ -14,7 +14,7 @@ public abstract class AutoDisambiguator {
     protected FsmMorphologicalAnalyzer morphologicalAnalyzer;
     protected RootWordStatistics rootWordStatistics;
 
-    private static boolean searchForSecondSingularPerson(FsmParseList[] fsmParses) {
+    private static boolean isAnyWordSecondPerson(FsmParseList[] fsmParses) {
         int count = 0;
         for (FsmParseList fsmPars : fsmParses) {
             boolean secondSingular = false;
@@ -31,7 +31,7 @@ public abstract class AutoDisambiguator {
         return count >= 2;
     }
 
-    private static boolean searchForNumberOfNoun(int index, ArrayList<FsmParse> correctParses) {
+    private static boolean isPossessivePlural(int index, ArrayList<FsmParse> correctParses) {
         for (int i = index - 1; i >= 0; i--) {
             if (correctParses.get(i).isNoun()) {
                 return correctParses.get(i).isPlural();
@@ -48,12 +48,28 @@ public abstract class AutoDisambiguator {
         return map.max();
     }
 
+    private static boolean isBeforeLastWord(int index, FsmParseList[] fsmParses){
+        return index + 2 == fsmParses.length;
+    }
+
+    private static boolean isNextWordNoun(int index, FsmParseList[] fsmParses){
+        return index + 1 < fsmParses.length && nextWordPos(fsmParses[index + 1]).equals("NOUN");
+    }
+
+    private static boolean isFirstWord(int index){
+        return index == 0;
+    }
+
+    private static boolean isCapital(String surfaceForm){
+        return Word.isCapital(surfaceForm);
+    }
+
     public static String selectCaseForParseString(String parseString, int index, FsmParseList[] fsmParses, ArrayList<FsmParse> correctParses) {
         String surfaceForm = fsmParses[index].getFsmParse(0).getSurfaceForm();
         switch (parseString) {
             /* kısmını, duracağını, grubunun */
             case "P2SG$P3SG":
-                if (searchForSecondSingularPerson(fsmParses)) {
+                if (isAnyWordSecondPerson(fsmParses)) {
                     return "P2SG";
                 }
                 return "P3SG";
@@ -62,21 +78,21 @@ public abstract class AutoDisambiguator {
                 return "DET";
                 /* tahminleri, işleri, hisseleri */
             case "A3PL+P3PL+NOM$A3PL+P3SG+NOM$A3PL+PNON+ACC$A3SG+P3PL+NOM":
-                if (searchForNumberOfNoun(index, correctParses)) {
+                if (isPossessivePlural(index, correctParses)) {
                     return "A3SG+P3PL+NOM";
                 }
                 return "A3PL+P3SG+NOM";
                 /* Ocak, Cuma, ABD */
             case "A3SG$PROP+A3SG":
                 if (index > 0) {
-                    if (Word.isCapital(surfaceForm)) {
+                    if (isCapital(surfaceForm)) {
                         return "PROP+A3SG";
                     }
                     return "A3SG";
                 }
                 /* şirketin, seçimlerin, borsacıların, kitapların */
             case "P2SG+NOM$PNON+GEN":
-                if (searchForSecondSingularPerson(fsmParses)) {
+                if (isAnyWordSecondPerson(fsmParses)) {
                     return "P2SG+NOM";
                 }
                 return "PNON+GEN";
@@ -98,45 +114,41 @@ public abstract class AutoDisambiguator {
                     }
                 }
             case "ADJ$NOUN+A3SG+PNON+NOM":
-                if (index + 1 < fsmParses.length) {
-                    if ("NOUN".equals(nextWordPos(fsmParses[index + 1]))) {
-                        return "ADJ";
-                    }
+                if (isNextWordNoun(index, fsmParses)) {
+                    return "ADJ";
                 }
                 return "NOUN+A3SG+PNON+NOM";
                 /* fanatiklerini, senetlerini, olduklarını */
             case "A3PL+P2SG$A3PL+P3PL$A3PL+P3SG$A3SG+P3PL":
-                if (searchForSecondSingularPerson(fsmParses)) {
+                if (isAnyWordSecondPerson(fsmParses)) {
                     return "A3PL+P2SG";
                 }
-                if (searchForNumberOfNoun(index, correctParses)) {
+                if (isPossessivePlural(index, correctParses)) {
                     return "A3SG+P3PL";
                 } else {
                     return "A3PL+P3SG";
                 }
             case "ADJ$NOUN+PROP+A3SG+PNON+NOM":
                 if (index > 0) {
-                    if (Word.isCapital(surfaceForm)) {
+                    if (isCapital(surfaceForm)) {
                         return "NOUN+PROP+A3SG+PNON+NOM";
                     }
                     return "ADJ";
                 }
                 /* BU, ŞU */
             case "DET$PRON+DEMONSP+A3SG+PNON+NOM":
-                if (index + 1 < fsmParses.length) {
-                    if ("NOUN".equals(nextWordPos(fsmParses[index + 1]))) {
-                        return "DET";
-                    }
+                if (isNextWordNoun(index, fsmParses)) {
+                    return "DET";
                 }
                 return "PRON+DEMONSP+A3SG+PNON+NOM";
                 /* gelebilir */
             case "AOR+A3SG$AOR^DB+ADJ+ZERO":
-                if (index + 2 == fsmParses.length) {
+                if (isBeforeLastWord(index, fsmParses)) {
                     return "AOR+A3SG";
-                } else if (index == 0) {
+                } else if (isFirstWord(index)) {
                     return "AOR^DB+ADJ+ZERO";
                 } else {
-                    if ("NOUN".equals(nextWordPos(fsmParses[index + 1]))) {
+                    if (isNextWordNoun(index, fsmParses)) {
                         return "AOR^DB+ADJ+ZERO";
                     } else {
                         return "AOR+A3SG";
@@ -145,25 +157,26 @@ public abstract class AutoDisambiguator {
             case "ADV$NOUN+A3SG+PNON+NOM":
                 return "ADV";
             case "ADJ$ADV":
-                if (nextWordPos(fsmParses[index + 1]).equals("NOUN")) {
+                if (isNextWordNoun(index, fsmParses)) {
                     return "ADJ";
                 }
                 return "ADV";
             case "P2SG$PNON":
-                if (searchForSecondSingularPerson(fsmParses)) {
+                if (isAnyWordSecondPerson(fsmParses)) {
                     return "P2SG";
                 }
                 return "PNON";
                 /* etti, kırdı */
             case "NOUN+A3SG+PNON+NOM^DB+VERB+ZERO$VERB+POS":
-                if (index + 2 == fsmParses.length) {
+                if (isBeforeLastWord(index, fsmParses)) {
                     return "VERB+POS";
                 }
                 /* ile */
             case "CONJ$POSTP+PCNOM":
                 return "POSTP+PCNOM";
+                /* gelecek */
             case "POS+FUT+A3SG$POS^DB+ADJ+FUTPART+PNON":
-                if (index + 2 == fsmParses.length) {
+                if (isBeforeLastWord(index, fsmParses)) {
                     return "POS+FUT+A3SG";
                 }
                 return "POS^DB+ADJ+FUTPART+PNON";

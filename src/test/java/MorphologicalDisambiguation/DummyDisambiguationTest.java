76bafdc89e134040b23c7a08af17050c04f8771c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-09-16 00:14:19 +0300

    Updated DataCollector dependency.

--- src/test/java/MorphologicalDisambiguation/DummyDisambiguationTest.java
+++ src/test/java/MorphologicalDisambiguation/DummyDisambiguationTest.java
@@ -36,7 +36,7 @@ public class DummyDisambiguationTest {
             }
         }
         assertEquals(0.8618, (correctRoot + 0.0) / corpus.numberOfWords(), 0.002);
-        assertEquals(0.7046, (correctParse + 0.0) / corpus.numberOfWords(), 0.002);
+        assertEquals(0.7025, (correctParse + 0.0) / corpus.numberOfWords(), 0.002);
     }
 
 }
